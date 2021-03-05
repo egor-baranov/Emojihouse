3853491aa4622c92b5b6d0f29184ca5985ce7204
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-03-05 22:08:51 +0300

    🎉 Initial commit

---
+++ app/src/main/java/com/kepler88d/emojihouse/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kepler88d.emojihouse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
