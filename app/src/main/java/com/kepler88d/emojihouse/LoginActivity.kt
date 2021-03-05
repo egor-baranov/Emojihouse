285de555c2342360a31f123f6451ce426fd80ecd
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-03-06 02:27:32 +0300

    ✨ Login button functionality

--- app/src/main/java/com/kepler88d/emojihouse/LoginActivity.kt
+++ app/src/main/java/com/kepler88d/emojihouse/LoginActivity.kt
@@ -1,21 +1,21 @@
 package com.kepler88d.emojihouse
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
+import com.google.android.material.button.MaterialButton
 import com.google.android.material.textfield.TextInputLayout
 import com.kepler88d.emojihouse.databinding.ActivityLoginBinding
 
 
 class LoginActivity : AppCompatActivity() {
-    lateinit var binding : ActivityLoginBinding
+    lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
-       // setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_login)
 
         findViewById<TextInputLayout>(R.id.textField).editText!!.addTextChangedListener(object :
             TextWatcher {
@@ -24,8 +24,15 @@ class LoginActivity : AppCompatActivity() {
 
             override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
 
-                findViewById<TextView>(R.id.textView).text = "\uD83D\uDC4B Hello${if(s.isNotBlank()) ", ${s.trim()}" else ""}."
+                findViewById<TextView>(R.id.textView).text =
+                    "\uD83D\uDC4B Hello${if (s.isNotBlank()) ", ${s.trim()}" else ""}."
+                findViewById<TextView>(R.id.textView).text =
+                    "\uD83D\uDC4B Hello${if (s.isNotBlank()) ", ${s.trim()}" else ""}."
             }
         })
+
+        findViewById<MaterialButton>(R.id.loginButton).setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/kepler88d/emojihouse/MainActivity.kt
+++ app/src/main/java/com/kepler88d/emojihouse/MainActivity.kt
@@ -2,6 +2,9 @@ package com.kepler88d.emojihouse
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import com.kepler88d.emojihouse.databinding.ActivityMainBinding
 
@@ -10,9 +13,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        for (i in 0..50) {
+            addChat("chat $i", (5 * i + 327374) % 84)
+        }
     }
 
-    private fun createChat() {
-
+    private fun addChat(chatName: String, memberCount: Int) {
+        val newView =
+            LayoutInflater.from(applicationContext).inflate(R.layout.chat_item, null, false)
+        newView.findViewWithTag<TextView>("channelName").text = chatName
+        newView.findViewWithTag<TextView>("memberCount").text = "$memberCount members"
+        findViewById<LinearLayout>(R.id.chatList).addView(newView)
     }
 }
