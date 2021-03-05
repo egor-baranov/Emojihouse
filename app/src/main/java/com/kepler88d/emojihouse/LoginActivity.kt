0e90122b41fca2b66489fac9088bfe3c24afbfbf
VITYQ <vitaly.sorokin14@gmail.com>
2021-03-06 00:55:37 +0300

    Imported Android

--- app/src/main/java/com/kepler88d/emojihouse/LoginActivity.kt
+++ app/src/main/java/com/kepler88d/emojihouse/LoginActivity.kt
@@ -6,8 +6,11 @@ import android.text.TextWatcher
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import android.widget.Toast
 import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.database.FirebaseDatabase
 import com.kepler88d.emojihouse.databinding.ActivityLoginBinding
+import java.util.*
 
 
 class LoginActivity : AppCompatActivity() {
@@ -15,16 +18,42 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
-       // setContentView(R.layout.activity_login)
+        attachTextChanger()
+        addLoginButtonListener()
+    }
 
-        findViewById<TextInputLayout>(R.id.textField).editText!!.addTextChangedListener(object :
+    private fun addLoginButtonListener() {
+        binding.loginButton.setOnClickListener {
+            val text = binding.textFieldUsername.editText?.text
+            if(text.isNullOrEmpty()){
+                binding.textFieldUsername.error = "Input your nickname"
+            }
+            else{
+                val filename = UUID.randomUUID().toString()
+                val ref = FirebaseDatabase.getInstance().getReference("/users/$filename")
+                val user = User(text.toString(), "")
+                ref.setValue(user)
+                    .addOnSuccessListener {
+                        Toast.makeText(this, "success", Toast.LENGTH_SHORT).show()
+                    }
+                    .addOnFailureListener{
+                        Toast.makeText(this, "failed", Toast.LENGTH_SHORT).show()
+                    }
+            }
+        }
+    }
+    data class User(
+        val nickname : String,
+        val profile_img : String = ""
+    )
+    private fun attachTextChanger(){
+        binding.textFieldUsername.editText!!.addTextChangedListener(object :
             TextWatcher {
             override fun afterTextChanged(s: Editable) {}
             override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
-
             override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-
-                findViewById<TextView>(R.id.textView).text = "\uD83D\uDC4B Hello${if(s.isNotBlank()) ", ${s.trim()}" else ""}."
+                binding.textFieldUsername.error = null
+                binding.textView.text = "\uD83D\uDC4B Hello${if(s.isNotBlank()) ", ${s.trim()}" else ""}."
             }
         })
     }
