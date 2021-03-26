df069e1f73f67811c816595449f124a7a78e9ae1
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-03-27 01:48:00 +0300

    🔨 Done some refactoring stuff in MainActivity.kt

--- app/src/main/java/com/kepler88d/emojihouse/MainActivity.kt
+++ app/src/main/java/com/kepler88d/emojihouse/MainActivity.kt
@@ -2,13 +2,11 @@ package com.kepler88d.emojihouse
 
 import android.app.AlertDialog
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.widget.LinearLayout
 import android.widget.TextView
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.textfield.TextInputLayout
@@ -42,116 +40,110 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-
         fetchRooms()
 
-
-
-        val currentActivity = this
         binding.floatingActionButton.setOnClickListener {
-            MaterialAlertDialogBuilder(currentActivity).setTitle("Choose an action 🤔")
-                .setItems(arrayOf("😎 Create chat", "🥳 Join Chat")) { dialog, which ->
-                    if(which == 0){
-                        startActivity(Intent(this, AddNewRoomActivity::class.java))
-                    }
-                    else if(which == 1){
-                        val dialogInflater = LayoutInflater.from(this).inflate(R.layout.dialog_password, null)
-                        val passDialog = dialogInflater.findViewById(R.id.textInputLayout_EnterFormPassword) as TextInputLayout
-
-                        val builder = AlertDialog.Builder(this)
-                            .setView(dialogInflater)
-                            .setTitle("Enter invite code")
-                            .setNegativeButton("Close"){dialog, which ->  }
-                            .setPositiveButton("Join"){dialog, which ->
-                                val passwordRoom = passDialog.editText?.text.toString()
-                                val ref = FirebaseDatabase.getInstance(url).getReference("/rooms")
-                                ref.addListenerForSingleValueEvent(object : ValueEventListener{
-                                    override fun onDataChange(snapshot: DataSnapshot) {
-
-                                        snapshot.children.forEach {
-                                            if(it.child("password").getValue() == passwordRoom){
-                                                val refSubscribeUser = FirebaseDatabase.getInstance(url).getReference("/users/${userData.id}/subscriptions")
-                                                refSubscribeUser.child(it.key.toString()).setValue("")
-                                                ref.child(it.key.toString()).child("members").child(userData.id).setValue("")
-                                                val name = it.child("roomName").getValue().toString()
-                                                val id = it.key.toString()
-                                                val count = it.child("members").childrenCount.toInt()
-                                                val pic = it.child("picture").getValue().toString()
-                                                addChat(name, count, id, pic)
-                                                return@forEach
-                                            }
-                                        }
-                                    }
-
-                                    override fun onCancelled(error: DatabaseError) {}
-                                })
-                            }
-                        builder.create().show()
-                    }
-
-
-                }
-                .show()
+            showMenuDialog()
         }
     }
 
-    private fun fetchRooms(){
-        val rooms = mutableListOf<String>()
-        val ref = FirebaseDatabase.getInstance(url).getReference("/users/${userData.id}/subscriptions")
-        ref.addListenerForSingleValueEvent(object : ValueEventListener{
-            override fun onDataChange(snapshot: DataSnapshot) {
-                snapshot.children.forEach {
-                    val id = it.key
-                    Log.d("checkfetch", "$id")
-                    rooms.add(id!!)
-                }
-                val refRooms = FirebaseDatabase.getInstance(url).getReference("/rooms")
-                rooms.forEach {
-                    Log.d("checkfetch", " 1 $it")
-                    val room = refRooms
-                    room.addListenerForSingleValueEvent(object : ValueEventListener{
-                        override fun onDataChange(snapshot: DataSnapshot) {
-                            val name = snapshot.child(it).child("roomName").getValue().toString()
-                            var count = 0
-                            val pic = snapshot.child(it).child("password").getValue().toString()
-                            val id = snapshot.child(it).key.toString()
-//                            if(name.isNullOrEmpty()){
-                                snapshot.child(it).child("members").children.forEach {
-                                    count++
-                                }
-                                Log.d("checkfetch", "$name $count $id")
-                                addChat(name, count, id, pic)
-//                            }
-
+    private fun showMenuDialog() {
+        MaterialAlertDialogBuilder(this).setTitle("Choose an action 🤔")
+            .setItems(arrayOf("😎 Create chat", "🥳 Join Chat")) { _, which ->
+                if (which == 0) {
+                    startActivity(Intent(this, AddNewRoomActivity::class.java))
+                } else {
+                    val builder = AlertDialog.Builder(this)
+                        .setView(
+                            LayoutInflater.from(this)
+                                .inflate(R.layout.dialog_password, null)
+                        )
+                        .setTitle("Enter invite code")
+                        .setNegativeButton("Close") { _, _ -> }
+                        .setPositiveButton("Join") { _, _ ->
+                            joinRoom()
                         }
-
-                        override fun onCancelled(error: DatabaseError) {
-
-                        }
-                    })
-
+                    builder.create().show()
                 }
-
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-
-            }
-        })
-        ref.onDisconnect()
-
-
+            }.show()
     }
 
-    private fun addChat(chatName: String, memberCount: Int, id: String, pic : String) {
+    private fun joinRoom() {
+        val passwordRoom = (
+                LayoutInflater.from(this).inflate(R.layout.dialog_password, null)
+                    .findViewById(R.id.textInputLayout_EnterFormPassword) as TextInputLayout
+                )
+            .editText?.text.toString()
+
+        val ref = FirebaseDatabase.getInstance(url).getReference("/rooms")
+        ref.addListenerForSingleValueEvent(object : ValueEventListener {
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                snapshot.children.forEach {
+                    if (it.child("password").value == passwordRoom) {
+                        val refSubscribeUser = FirebaseDatabase.getInstance(url)
+                            .getReference("/users/${userData.id}/subscriptions")
+                        refSubscribeUser.child(it.key.toString()).setValue("")
+                        ref.child(it.key.toString()).child("members").child(userData.id)
+                            .setValue("")
+
+                        addChat(
+                            it.child("roomName").value.toString(),
+                            it.child("members").childrenCount.toInt(),
+                            it.key.toString(),
+                            it.child("picture").value.toString()
+                        )
+                        return@forEach
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+        })
+    }
+
+    private fun fetchRooms() {
+        val rooms = mutableListOf<String>()
+        val ref = FirebaseDatabase.getInstance(url).getReference(
+            "/users/${userData.id}/subscriptions"
+        )
+        ref.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                snapshot.children.forEach {
+                    rooms.add(it.key!!)
+                }
+
+                rooms.forEach {
+                    FirebaseDatabase.getInstance(url).getReference("/rooms")
+                        .addListenerForSingleValueEvent(object : ValueEventListener {
+                            override fun onDataChange(snapshot: DataSnapshot) {
+                                val child = snapshot.child(it)
+                                addChat(
+                                    child.child("roomName").value.toString(),
+                                    child.child("members").children.count(),
+                                    child.key.toString(),
+                                    child.child("password").value.toString()
+                                )
+                            }
+
+                            override fun onCancelled(error: DatabaseError) {}
+                        })
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+        })
+        ref.onDisconnect()
+    }
+
+    private fun addChat(chatName: String, memberCount: Int, id: String, pic: String) {
         val newView =
             LayoutInflater.from(this).inflate(R.layout.chat_item, null, false)
         newView.findViewWithTag<TextView>("channelName").text = chatName
         newView.findViewWithTag<TextView>("memberCount").text = "$memberCount members"
         newView.findViewWithTag<TextView>("img").text = pic.take(2)
+
         newView.setOnClickListener {
-            Log.d("checkfetch", id)
             val intent = Intent(this, ChatActivity::class.java)
             intent.putExtra("id", id)
             startActivity(intent)
@@ -159,10 +151,4 @@ class MainActivity : AppCompatActivity() {
 
         findViewById<LinearLayout>(R.id.chatList).addView(newView)
     }
-
-    data class Rooms(
-        val id: String,
-        val roomName: String,
-        val password: String
-    )
 }
